7830c1d3b2fcca00d0b6b32a074dde13fc02dabb
chetantalele <chetantalele06@gmail.com>
2026-01-17 20:46:25 +0530

    final update

--- scripts/inject.sh
+++ scripts/inject.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-sed -i "s|IMAGE_URI|$IMAGE_URI|g" k8s/deployment.yaml
-sed -i "s|APP_NAME|$APP_NAME|g" k8s/ingress.yaml
-sed -i "s|DOMAIN_NAME|$DOMAIN_NAME|g" k8s/ingress.yaml
+set -e
+
+: "${APP_NAME:?APP_NAME not set}"
+: "${IMAGE_URI:?IMAGE_URI not set}"
+: "${DOMAIN_NAME:?DOMAIN_NAME not set}"
+
+find k8s -type f -name "*.yaml" -exec sed -i \
+  -e "s|__APP_NAME__|$APP_NAME|g" \
+  -e "s|__IMAGE_URI__|$IMAGE_URI|g" \
+  -e "s|__DOMAIN_NAME__|$DOMAIN_NAME|g" {} \;
